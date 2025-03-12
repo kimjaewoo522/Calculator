bab8a16075f95ba5da3d50d851160ef0d9a6be40
kimjaewoo522 <kimjaewoo522@gmail.com>
2025-03-13 00:48:35 +0900

    필수구현기능 LV1 구현

--- Calculator.playground/Contents.swift
+++ Calculator.playground/Contents.swift
@@ -1,3 +1,23 @@
-import UIKit
+class Calculator {
+    func calculate(oper: String, firstNumber: Int, secondNumber: Int) -> Int {
+        var result: Int
+        
+        if oper == "+" {
+            result = firstNumber + secondNumber
+        } else if oper == "-" {
+            result = firstNumber - secondNumber
+        } else if oper == "*" {
+            result = firstNumber * secondNumber
+        } else {
+            result = firstNumber / secondNumber
+        }
+        
+        return result
+    }
+}
 
-var greeting = "Hello, playground"
+let calculator = Calculator()
+print(calculator.calculate(oper: "+", firstNumber: 10, secondNumber: 20))
+print(calculator.calculate(oper: "-", firstNumber: 20, secondNumber: 50))
+print(calculator.calculate(oper: "*", firstNumber: 20, secondNumber: 50))
+print(calculator.calculate(oper: "/", firstNumber: 20, secondNumber: 50))
