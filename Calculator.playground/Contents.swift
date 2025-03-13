6048a8f20c67e854a2ed3860ad92f4b854f7b609
kimjaewoo522 <kimjaewoo522@gmail.com>
2025-03-14 01:49:52 +0900

    LV3 구현
    관계 맺기, 예외처리

--- Calculator.playground/Contents.swift
+++ Calculator.playground/Contents.swift
@@ -1,26 +1,67 @@
 class Calculator {
-    func calculate(oper: String, firstNumber: Int, secondNumber: Int) -> Int {
-        var result: Int
+    var addOperation = AddOperation()
+    var subtractOperation = SubtractOperation()
+    var multiplyOperation = MultiplyOperation()
+    var divideOperation = DivideOperation()
+    
+    func calculate(oper: String, firstNumber: Double, secondNumber: Double) -> String {
+        var result: Double
         
         if oper == "+" {
-            result = firstNumber + secondNumber
+            result = addOperation.add(firstNumber: firstNumber, secondNumber: secondNumber)
         } else if oper == "-" {
-            result = firstNumber - secondNumber
+            result = subtractOperation.sub(firstNumber: firstNumber, secondNumber: secondNumber)
         } else if oper == "*" {
-            result = firstNumber * secondNumber
+            result = multiplyOperation.multi(firstNumber: firstNumber, secondNumber: secondNumber)
         } else if oper == "/"{
-            result = firstNumber / secondNumber
+            if secondNumber == 0 {
+                return "0으로 나눌 수 없음!"
+            }
+            result = divideOperation.div(firstNumber: firstNumber, secondNumber: secondNumber)
+        } else if oper == "%" {
+            if secondNumber == 0 {
+                return "0으로 나눌 수 없음!"
+            }
+            result = firstNumber.truncatingRemainder(dividingBy: secondNumber)
         } else {
-            result = firstNumber % secondNumber
+            return "확인할 수 없는 연산자"
         }
         
-        return result
+        return "\(result)"
     }
 }
+
+class AddOperation {
+    func add(firstNumber: Double, secondNumber: Double) -> Double {
+        return firstNumber + secondNumber
+    }
+}
+
+class SubtractOperation {
+    func sub(firstNumber: Double, secondNumber: Double) -> Double {
+        return firstNumber - secondNumber
+    }
+    
+}
+
+class MultiplyOperation {
+    func multi(firstNumber: Double, secondNumber: Double) -> Double {
+        return firstNumber * secondNumber
+    }
+    
+}
+
+class DivideOperation {
+    func div(firstNumber: Double, secondNumber: Double) -> Double {
+        return firstNumber / secondNumber
+    }
+}
+
 
 let calculator = Calculator()
 print(calculator.calculate(oper: "+", firstNumber: 10, secondNumber: 20))
 print(calculator.calculate(oper: "-", firstNumber: 20, secondNumber: 50))
 print(calculator.calculate(oper: "*", firstNumber: 20, secondNumber: 50))
-print(calculator.calculate(oper: "/", firstNumber: 20, secondNumber: 50))
-print(calculator.calculate(oper: "%", firstNumber: 70, secondNumber: 50))
+print(calculator.calculate(oper: "/", firstNumber: 20, secondNumber: 0))
+print(calculator.calculate(oper: "%", firstNumber: 2.1, secondNumber: 2.4))
+print(calculator.calculate(oper: "ㅁㅇㄹ", firstNumber: 70, secondNumber: 50))
